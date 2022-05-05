ed6ac6e1fcaa203f86ef363e54edbd3d3b527ef1
walle <dlauddn95@gmail.com>
2022-05-06 00:01:07 +0900

    :>

---
+++ src/test/java/hello/core/autuwried/AutowriedTest.java
@@ -0,0 +1,37 @@
+package hello.core.autuwried;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutowriedTest {
+    @Test
+    void AutowriedTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean{
+        //자동 주입할 대상이 없으면 호출 자체가 안된다.
+        @Autowired(required = false)
+        public void setNoBean1(Member member) {
+            System.out.println("setNoBean1 = " + member);
+        }
+
+        //null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("setNoBean2 = " + member);
+        }
+
+        //Optional.empty 호출
+        @Autowired(required = false)
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("setNoBean3 = " + member);
+        }
+    }
+}
